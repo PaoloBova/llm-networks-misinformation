26e2d25f6b394503b4112a4b77540b8ec8fe9859
Paolo Bova <paolobova@protonmail.com>
2024-07-09 21:53:35 +0100

    Use new data utilities to save data and plots in script1.py

--- scripts/script1.py
+++ scripts/script1.py
@@ -5,6 +5,7 @@ import networkx
 
 from src.agent import Agent
 import src.core
+import src.data_utils
 from src.models import DebateManager
 import src.plot_utils
 import src.prompts as prompts
@@ -12,6 +13,8 @@ import src.prompts as prompts
 # Load API key as environment variable from .env file
 load_dotenv()
 API_KEY = os.getenv("OPENAI_API_KEY")
+
+current_commit = src.data_utils.get_current_git_commit()
 
 # Adjustable parameters
 NUM_AGENTS = 4
@@ -39,12 +42,26 @@ params = {"num_agents": NUM_AGENTS,
           "num_simulations": NUM_SIMULATIONS,
           "temperature": TEMPERATURE,
           "api_key": API_KEY,
+          "commit": current_commit,
           "prompt_functions": prompt_functions,
           "seed": seed,
           "model_class": DebateManager,
           "agent_class": Agent,
           "graph": graph}
 
+# We create the simulation id before any random seeds are set
+simulation_id = src.data_utils.create_id()
+data_dir = f"data/{simulation_id}"
+plots_dir = f"plots/{simulation_id}"
+
 results = src.core.run_multiple_simulations(params)
 
+src.data_utils.save_data(results, data_dir=data_dir)
+
 plot1 = src.plot_utils.plot_simulation_results({**params, "results": results})
+
+# Warning: save_name does not support all types of values
+simulation_stub = src.data_utils.save_name(params, sensitive_keys=["api_key"])
+
+plots = {f"simulation_results_{simulation_stub}": plot1}
+src.data_utils.save_plots(plots, plots_dir=plots_dir)
